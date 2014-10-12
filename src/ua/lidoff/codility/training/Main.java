934bdf142ff062b213c924b7f34d706e3434541c
Alexey Lidov <alexlidoff@gmail.com>
2014-10-12 14:10:12 +0300

    initial commit

---
+++ src/ua/lidoff/codility/training/Main.java
@@ -0,0 +1,34 @@
+package ua.lidoff.codility.training;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        final int n = 10;
+
+        int total1 = calcTotalSlow(n);
+        System.out.println(total1);
+
+        int total2 = calcTotalFast(n);
+        System.out.println(total2);
+
+    }
+
+
+    public static int calcTotalSlow(int n) {
+
+        int total = 0;
+        for (int i = 1; i <= n; i++) {
+            total += i;
+        }
+        return total;
+
+    }
+
+    public static int calcTotalFast(int n) {
+
+        return n * (n + 1) / 2;
+
+    }
+
+}
